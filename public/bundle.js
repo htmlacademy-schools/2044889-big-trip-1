99fd3730cdf55a13e45be37b11cd66b536603bd5
SemenGladyshev <gladyshev15081508@gmail.com>
2022-02-23 14:08:47 +0500

    2.2

---
+++ public/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "C:4889-big-trip-1/src/main.js":
+/*!**************************************!*\
+  !*** C:4889-big-trip-1/src/main.js ***!
+  \**************************************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"C:4889-big-trip-1/src/main.js\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"c:\" URIs.\n    at C:\\2044889-big-trip-1\\node_modules\\webpack\\lib\\NormalModule.js:825:25\n    at Hook.eval [as callAsync] (eval at create (C:\\2044889-big-trip-1\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (C:\\2044889-big-trip-1\\node_modules\\tapable\\lib\\Hook.js:18:14)\n    at Object.processResource (C:\\2044889-big-trip-1\\node_modules\\webpack\\lib\\NormalModule.js:822:8)\n    at processResource (C:\\2044889-big-trip-1\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\2044889-big-trip-1\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\2044889-big-trip-1\\node_modules\\loader-runner\\lib\\LoaderRunner.js:397:2)\n    at NormalModule._doBuild (C:\\2044889-big-trip-1\\node_modules\\webpack\\lib\\NormalModule.js:812:3)\n    at NormalModule.build (C:\\2044889-big-trip-1\\node_modules\\webpack\\lib\\NormalModule.js:956:15)\n    at C:\\2044889-big-trip-1\\node_modules\\webpack\\lib\\Compilation.js:1367:12");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["C:4889-big-trip-1/src/main.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
