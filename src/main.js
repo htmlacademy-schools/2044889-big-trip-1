ee4f42b0b22cf1f142d14af51f718f8a2d2dfcad
SemenGladyshev <gladyshev15081508@gmail.com>
2022-03-03 12:12:58 +0500

    Добавил разметку

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import {createSiteMenuTemplate} from './view/site-menu-view.js';
 import { renderTemplate, RenderPosition } from './render.js';
 import { createFilterTemplate } from './view/filter-view.js';
+import { createLoadingTemplate } from './view/loading-view.js';
+import { createListEmptyTemplate } from './view/list-empty-view.js';
 
 
 const siteMainElement = document.querySelector('.main');
@@ -8,3 +10,5 @@ const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
 renderTemplate(siteHeaderElement, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(siteMainElement, createFilterTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createLoadingTemplate(), RenderPosition.BEFOREBEGIN);
+renderTemplate(siteMainElement, createListEmptyTemplate(), RenderPosition.BEFOREBEGIN);
