fe9f57c5482639ce300b351266c59f27af668621
SemenGladyshev <gladyshev15081508@gmail.com>
2022-06-11 23:54:15 +0500

    api fix

--- src/service/api-service.js
+++ src/service/api-service.js
@@ -1,10 +1,9 @@
-const  Method = {
+const Method = {
   PUT: 'PUT',
   GET: 'GET',
   POST: 'POST',
   DELETE: 'DELETE',
 };
-
 export default class ApiService {
   #endPoint = null;
   #authorization = null;
@@ -83,10 +82,10 @@ export default class ApiService {
 
   #adaptToServer = (point) => {
     const adaptedPoint = {...point,
-      'base-price': point.basePrice,
-      'date-from': point.dateFrom,
-      'date-to': point.dateTo,
-      'is-favorite': point.isFavorite,
+      'base_price': point.basePrice,
+      'date_from': point.dateFrom,
+      'date_to': point.dateTo,
+      'is_favorite': point.isFavorite,
     };
 
     delete adaptedPoint.basePrice;
